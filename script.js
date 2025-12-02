60dce8d829e1298d448dfbfcddf9241b565caab3
MrTrupen <MrTrupen@gmail.com>
2025-12-02 13:25:07 +0100

    Day-1 (#1)
    * world css
    
    * init square1
    
    * rotate square
    
    * clear for the prep
    
    * init js
    
    * autoformat css
    
    * key listening in js
    
    * autoformat index.html
    
    * key up script
    
    * player movement
    
    * js movement updates, probably still broken
    
    * js final fix
    
    * jump fix
    
    * idk whats going on with this js
    
    * fix overflow outside the game border
    
    * make jump go up

---
+++ script.js
@@ -0,0 +1,73 @@
+function player(x, y, z, rx, ry) {
+  this.x = x;
+  this.y = y;
+  this.z = z;
+  this.rx = rx;
+  this.ry = ry;
+}
+
+//Variables for movement
+var PressLeft = 0;
+var PressRight = 0;
+var PressForward = 0;
+var PressBack = 0;
+var PressUp = 0;
+
+//if the key is pressed
+document.addEventListener("keydown", (event) => {
+  if (event.key == "w") {
+    PressForward = 1;
+  }
+  if (event.key == "s") {
+    PressBack = 1;
+  }
+  if (event.key == "d") {
+    PressRight = 1;
+  }
+  if (event.key == "a") {
+    PressLeft = 1;
+  }
+  if (event.key == " ") {
+    PressUp = 1;
+  }
+});
+
+// if the key is released
+document.addEventListener("keyup", (event) => {
+  if (event.key == "w") {
+    PressForward = 0;
+  }
+  if (event.key == "s") {
+    PressBack = 0;
+  }
+  if (event.key == "d") {
+    PressRight = 0;
+  }
+  if (event.key == "a") {
+    PressLeft = 0;
+  }
+  if (event.key == " ") {
+    PressUp = 0;
+  }
+});
+
+var pawn = new player(0, 0, 0, 0, 0);
+var world = document.getElementById("world");
+
+function update() {
+  //count movement
+  dx = PressRight - PressLeft;
+  dz = -(PressForward - PressBack);
+  dy = -PressUp;
+
+  //add movement to the coordinates
+  pawn.x = pawn.x + dx;
+  pawn.y = pawn.y + dy;
+  pawn.z = pawn.z + dz;
+
+  //change coordinates of the world
+  world.style.transform =
+    "translate3d(" + -pawn.x + "px," + -pawn.y + "px," + -pawn.z + "px)";
+}
+
+TimerGame = setInterval(update, 10);
