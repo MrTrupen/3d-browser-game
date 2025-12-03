e89227d972fee64a85cd290bf71df17d4147e22c
school-lenovo-trupen <MrTrupen@gmail.com>
2025-12-03 19:27:30 +0200

    add cube

--- script.js
+++ script.js
@@ -14,6 +14,13 @@ var map = [
   [-1000, 0, 0, 0, -90, 0, 2000, 200, "url('Patterns/hedge.jpg')"], // left wall
   [0, 100, 0, 90, 0, 0, 2000, 2000, "url('Patterns/tiles.jpg')"], // ground
   [0, 0, -1000, 0, 0, 0, 83, 180, "url('Patterns/door.png')"], // door
+
+  [0, 0, -100, 0, 0, 0, 200, 200, "url('Patterns/hedge.jpg')"], // front walls
+  [0, 0, 100, 0, 0, 0, 200, 200, "url('Patterns/hedge.jpg')"], // back wall
+  [100, 0, 0, 0, 90, 0, 200, 200, "url('Patterns/hedge.jpg')"], // right wall
+  [-100, 0, 0, 0, -90, 0, 200, 200, "url('Patterns/hedge.jpg')"], // left wall
+  [0, 100, 0, 90, 0, 0, 200, 200, "url('Patterns/hedge.jpg')"], // top wall
+  [0, -100, 0, 90, 0, 0, 200, 200, "url('Patterns/hedge.jpg')"], // bottom wall
 ];
 
 //Variables for movement
