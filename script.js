68e2941bb8d5ba732640e31f47501475b23c5802
school-lenovo-trupen <MrTrupen@gmail.com>
2025-12-04 14:44:26 +0200

    add class for rectangle

--- script.js
+++ script.js
@@ -7,6 +7,29 @@ class player {
     this.rotation_y = rotation_y;
   }
 }
+class rectangle {
+  constructor(
+    x,
+    y,
+    z,
+    rotation_x,
+    rotation_y,
+    rotation_z,
+    width,
+    height,
+    pattern_path
+  ) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.rotation_x = rotation_x;
+    this.rotation_y = rotation_y;
+    this.rotation_z = rotation_z;
+    this.width = width;
+    this.height = height;
+    this.pattern_path = pattern_path;
+  }
+}
 
 //Variables for movement
 var press_left = 0;
