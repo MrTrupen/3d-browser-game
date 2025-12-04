7ad76539f64b1c47802c31f8629a60227100bbce
school-lenovo-trupen <MrTrupen@gmail.com>
2025-12-04 13:55:26 +0200

    add comment for the test cube

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ var map = [
   [0, 100, 0, 90, 0, 0, 2000, 2000, "url('Patterns/tiles.jpg')"], // ground
   [0, 0, -1000, 0, 0, 0, 83, 180, "url('Patterns/door.png')"], // door
 
+  // Test cube
   [0, 0, -100, 0, 0, 0, 200, 200, "url('Patterns/hedge.jpg')"], // front walls
   [0, 0, 100, 0, 0, 0, 200, 200, "url('Patterns/hedge.jpg')"], // back wall
   [100, 0, 0, 0, 90, 0, 200, 200, "url('Patterns/hedge.jpg')"], // right wall
