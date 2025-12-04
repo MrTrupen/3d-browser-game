e0d4f29df90b53f617992690ec787f53b735029b
school-lenovo-trupen <MrTrupen@gmail.com>
2025-12-04 18:10:54 +0200

    create menu.js and start implementing the GUI stuff

---
+++ scripts/menu.js
@@ -0,0 +1,6 @@
+//variables for navigation
+let menu1 = document.getElementById("menu1");
+let menu2 = document.getElementById("menu2");
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,7 @@ var press_sprint = 1;
 let mouse_x = 0;
 let mouse_y = 0;
 let is_mouse_locked = false;
+let can_lock = false;
 
 // Variable for HTML objects
 var world = document.getElementById("world");
@@ -17,10 +18,12 @@ var container = document.getElementById("container");
 
 // Mouse locking
 container.onclick = function () {
-  if (!is_mouse_locked) {
-    container.requestPointerLock();
-  } else {
-    document.exitPointerLock(); // Use document for exit
+  if (can_lock) {
+    if (!is_mouse_locked) {
+      container.requestPointerLock();
+    } else {
+      document.exitPointerLock(); // Use document for exit
+    }
   }
 };
 
