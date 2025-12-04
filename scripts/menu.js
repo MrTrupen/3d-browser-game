06645f0215aec62e8d719084b5cba7395dd74728
school-lenovo-trupen <MrTrupen@gmail.com>
2025-12-04 18:17:48 +0200

    logic for turning instruction on/off

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,3 +4,15 @@ let menu2 = document.getElementById("menu2");
 let button1 = document.getElementById("button1");
 let button2 = document.getElementById("button2");
 let button3 = document.getElementById("button3");
+
+// create navigation
+button2.onclick = function () {
+  menu1.style.display = "none";
+  menu2.style.display = "block";
+};
+
+// create navigation
+button3.onclick = function () {
+  menu1.style.display = "block";
+  menu2.style.display = "none";
+};
