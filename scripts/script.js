a090553264e5dcd5f2aad915323efa93dc1cb9bd
school-lenovo-trupen <MrTrupen@gmail.com>
2025-12-05 18:27:42 +0200

    changes to world generation logic

--- scripts/script.js
+++ scripts/script.js
@@ -129,35 +129,38 @@ function update() {
 }
 
 function create_new_world() {
-  for (let array_idx = 0; array_idx < objects.length; array_idx++) {
-    for (let object_idx = 0; object_idx < objects[array_idx].length; object_idx++) {
-      //create rectangles and styles
-      let newElement = document.createElement("div");
-      newElement.className = "square";
-      newElement.id = "square_" + array_idx + "_" + object_idx;
-      newElement.style.width = to_px(objects[array_idx][object_idx].width);
-      newElement.style.height = to_px(objects[array_idx][object_idx].height);
+  create_squares(boundries, "bondries");
+  create_squares(generate_maze(10, 200), "walls");
+}
 
-      // Apply textures based on surface type
-      newElement.style.backgroundImage = objects[array_idx][object_idx].pattern_path;
+function create_squares(squares, object_type) {
+  for (let object_idx = 0; object_idx < squares.length; object_idx++) {
+    //create rectangles and styles
+    let newElement = document.createElement("div");
+    newElement.className = object_type + " square";
+    newElement.id = object_type + object_idx;
+    newElement.style.width = to_px(squares[object_idx].width);
+    newElement.style.height = to_px(squares[object_idx].height);
 
-      newElement.style.transform =
-        "translate3d(" +
-        to_px(600 - objects[array_idx][object_idx].width / 2 + objects[array_idx][object_idx].x) +
-        "," +
-        to_px(400 - objects[array_idx][object_idx].height / 2 + objects[array_idx][object_idx].y) +
-        "," +
-        to_px(objects[array_idx][object_idx].z) +
-        ") rotateX(" +
-        objects[array_idx][object_idx].rotation_x +
-        "deg) rotateY(" +
-        objects[array_idx][object_idx].rotation_y +
-        "deg) rotateZ(" +
-        objects[array_idx][object_idx].rotation_z +
-        "deg)";
+    // Apply textures based on surface type
+    newElement.style.backgroundImage = squares[object_idx].pattern_path;
 
-      //insert rectangles into the world
-      world.append(newElement);
-    }
+    newElement.style.transform =
+      "translate3d(" +
+      to_px(600 - squares[object_idx].width / 2 + squares[object_idx].x) +
+      "," +
+      to_px(400 - squares[object_idx].height / 2 + squares[object_idx].y) +
+      "," +
+      to_px(squares[object_idx].z) +
+      ") rotateX(" +
+      squares[object_idx].rotation_x +
+      "deg) rotateY(" +
+      squares[object_idx].rotation_y +
+      "deg) rotateZ(" +
+      squares[object_idx].rotation_z +
+      "deg)";
+
+    //insert rectangles into the world
+    world.append(newElement);
   }
 }
